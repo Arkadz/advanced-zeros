e5d0fe0cddfb9742cee5d5891fc1828a6f0880ef
Arkadz <penkratpavel@gmail.com>
2018-10-04 21:22:37 +0300

    !

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  
+  let z, temp, tempBase, tempNumber;
+  let a = 0;
+  
+  tempNumber = number;
+  tempBase = base;
+
+  for (let i = 2; i <= base; i++) {
+    if (tempBase % i == 0) {
+      a = 0;
+      while (tempBase % i == 0) {
+      a++;
+      tempBase = Math.floor(tempBase / i);
+      }
+      z = 0;
+      temp = number;
+      while (temp / i > 0) {
+      z += Math.floor(temp / i);
+      temp = Math.floor(temp / i);
+      }
+      if (tempNumber > z / a) {
+      tempNumber = z / a;
+      }
+    }  
+  }
+return Math.floor(tempNumber);
 }
